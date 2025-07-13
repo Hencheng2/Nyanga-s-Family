89da945ad9a528ad0230525774e248dbe5010e58
Henry Ochieng <111287785+Hencheng2@users.noreply.github.com>
2025-07-13 18:52:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// This file can be expanded for more interactivity.
+// For now, it handles the basic button alerts.
+
+// You can add more complex JavaScript here later,
+// for example, to create dynamic galleries or video players.
